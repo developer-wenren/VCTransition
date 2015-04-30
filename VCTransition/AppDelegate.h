5f847d78847b43203f148c28c6a15888dc587019
wrcj <wrcj12138@163.com>
2015-04-30 17:08:10 +0800

    Initial Commit

---
+++ VCTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VCTransition
+//
+//  Created by zjsruxxxy3 on 15/4/30.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
